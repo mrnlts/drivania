e9b852c44558340f1ab6fb651feb28cb67b2ad1d
Marina Altés <mrnlts1@gmail.com>
2022-07-05 14:10:12 +0200

    Adds route to display create form, links view to main page.

--- src/Controller/RideServiceController.php
+++ src/Controller/RideServiceController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Crud;
+
+use App\Form\CrudType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RideServiceController extends AbstractController
 {
-    #[Route('/ride_service', name: 'app_ride_service')]
+    #[Route('/ride_service', name: 'main')]
     public function index(): Response
     {
         return $this->render('ride_service/index.html.twig', [
             'controller_name' => 'RideServiceController',
+        ]);
+    }
+
+    #[Route('/ride_service/create', name: 'create')]
+    public function create(Request $request){
+        $crud = new Crud();
+        $form = $this->createForm(CrudType::class, $crud);
+        $form->handleRequest($request);
+
+        return $this->render('ride_service/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
